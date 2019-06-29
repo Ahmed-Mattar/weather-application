9c48f9da9858a82f88fef3011b1e1827d59e80d2
Ahmed Mattar <AceMattar@gmail.com>
2019-06-29 05:41:43 +0200

    feat:Making HTTP requests

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout( ()=> {
+const url = 'https://api.darksky.net/forecast/fced9d8054cb4b4924abf70cb6423af9/37.8267,-122.4233';
 
-    console.log('2 seconds Timer')
-
-},  2000)
-
-setTimeout( ()=> {
-
-    console.log('0 seconds Timer')
-
-},  0)
-
-console.log('Stopping')
+request({url: url},(error, response,body)=>{
+    const data = JSON.parse(body);
+    console.log(data.currently)
+})
