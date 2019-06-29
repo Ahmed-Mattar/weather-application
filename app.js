59e368eccdcdb4f5ac41db2e472f9e9ffba53114
Ahmed Mattar <AceMattar@gmail.com>
2019-06-29 05:06:23 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Starting')
+
+setTimeout( ()=> {
+
+    console.log('2 seconds Timer')
+
+},  2000)
+
+setTimeout( ()=> {
+
+    console.log('0 seconds Timer')
+
+},  0)
+
+console.log('Stopping')
