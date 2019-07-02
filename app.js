342e45fdc62319938f5ab7e9c59f97f3302030e7
Ahmed Mattar <AceMattar@gmail.com>
2019-07-02 18:36:25 +0200

    feat: callback chaining and getting input location from user through cli

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('cairo', (error,data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
+const location = process.argv[2];
+
+if(!location){
+    console.log('Please provide an address')
+}else {
+
+    geocode(location, (error,data) => {
+
+        if (error) {
+             return console.log('Error', error)
+        }
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return console.log('Error', error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    
+    })
+
+}
 
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+
+
