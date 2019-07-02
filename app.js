11b693fae03e76c7eb7dc93a32a25a6896d73476
Ahmed Mattar <AceMattar@gmail.com>
2019-07-02 19:07:05 +0200

    feat: used object destructuring and property shorthand

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ if(!location){
     console.log('Please provide an address')
 }else {
 
-    geocode(location, (error,data) => {
+    geocode(location, (error,{latitude,longitude,location}) => {
 
         if (error) {
              return console.log('Error', error)
         }
         
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return console.log('Error', error)
             }
     
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,14 +4,14 @@ const request = require('request')
 
 const forecast = (latitude,longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/fced9d8054cb4b4924abf70cb6423af9/'+ latitude+ ',' +longitude+ '?units=si';
-    request({url:url, json:true}, (error,response) => {
+    request({url, json:true}, (error,{body}) => {
         if (error){
             callback('Unable to connect to weather service!',undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location',undefined)
         }
         else {
-            const msg = `${response.body.daily.data[0].summary} It's currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`
+            const msg = `${body.daily.data[0].summary} It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
             callback(undefined,msg)
         }
     })
