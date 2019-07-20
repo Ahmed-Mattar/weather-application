9e3f7c6077642e5a0c22c81af8dc5387827b427b
Ahmed Mattar <AceMattar@gmail.com>
2019-07-20 03:53:31 +0200

    feat:basic web-server structure

---
+++ web-server/src/app.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config 
+const publicDirectoryPath = path.join(__dirname, '..', 'public')
+const viewsPath = path.join(__dirname, '..', 'templates','views')
+const partialsPath = path.join(__dirname, '..', 'templates', 'partials')
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index' , {
+        title: 'Weather App',
+        name: 'Ahmed Mattar'
+    })
+})
+
+app.get('/about', (req,res)=>{
+
+    res.render('about', {
+        title:'About me',
+        name: 'Ahmed Mattar'
+    })
+
+})
+
+app.get('/help',(req,res)=>{
+     res.render('help', {
+         message:"This is the help message",
+         title:'Help',
+         name:'Ahmed Mattar'
+     })   
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.get('/help/*', (req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Ahmed Mattar',
+        errorMessage:'Help article not found'
+    })
+})
+
+app.get('*', (req, res) =>{
+    res.render('404',{
+        title:'404',
+        name:'Ahmed Mattar',
+        errorMessage:'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
